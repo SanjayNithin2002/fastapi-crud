bb60469fdc6cedf545c7414e9c754e8f7166fb23
SanjayNithin2002 <sanjay.nithin19@gmail.com>
2026-02-16 01:08:34 +0530

    jwtcleanup

--- app/api/dependencies/auth.py
+++ app/api/dependencies/auth.py
@@ -10,11 +10,16 @@ def get_payload(token: str = Depends(oauth2_scheme)):
     payload = jwt_auth.decode_token(token)
     return payload
 
-def require_scope(permission: str):
+def require_scope(user_id: str, permission: str):
     def checker(payload: dict = Depends(get_payload)):
         scopes = payload.get("scopes", [])
-
-        if permission not in scopes:
+        if isinstance(scopes, list):
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail=f"Token scopes are invalid"
+            )
+            
+        if user_id != payload.get(sub) and permission not in scopes:
             raise HTTPException(
                 status_code=status.HTTP_403_FORBIDDEN,
                 detail=f"Missing permission: {permission}"
